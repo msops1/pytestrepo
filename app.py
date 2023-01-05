b59b3f9a9a393da912d61b49c2d36242ea33d5d9
msops1 <121091556+msops1@users.noreply.github.com>
2023-01-05 10:56:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
+
 from flask import Flask
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
